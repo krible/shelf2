1fd6d188a22104dbefa77735b4408473aa3c31d8
Roman Teplov <teplov@gmail.com>
2016-02-26 19:00:21 +0300

    Update 2016-02-26T16:00:14.372Z

--- js/shelf2.js
+++ js/shelf2.js
@@ -9,4 +9,16 @@ $(function () {
         }
       }
     );
+
+    $(window).scroll(function() {
+      $('#kribleShelf').removeClass('fadeInUp');
+      $('#kribleShelf').addClass('fadeOutDown');
+      clearTimeout($.data(this, 'scrollTimer'));
+      $.data(this, 'scrollTimer', setTimeout(function() {
+          // do something
+          console.log("Haven't scrolled in 250ms!");
+          $('#kribleShelf').removeClass('fadeOutDown');
+          $('#kribleShelf').addClass('fadeInUp');
+      }, 250));
+});
 })
